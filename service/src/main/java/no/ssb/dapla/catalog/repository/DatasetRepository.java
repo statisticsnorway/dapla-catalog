d886e2daafefad17f07136926a06af3e31c7312d
Øyvind Strømmen <insula.ventus@gmail.com>
2019-12-18 14:01:36 +0100

    Implement a repository with create and get

---
+++ service/src/main/java/no/ssb/dapla/catalog/repository/DatasetRepository.java
@@ -0,0 +1,61 @@
+package no.ssb.dapla.catalog.repository;
+
+import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+import com.google.cloud.bigtable.data.v2.models.Query;
+import com.google.cloud.bigtable.data.v2.models.Row;
+import com.google.cloud.bigtable.data.v2.models.RowMutation;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import no.ssb.dapla.catalog.protobuf.Dataset;
+
+public class DatasetRepository {
+
+    private BigtableDataClient dataClient;
+
+    private static final String TABLE_ID = "dataset";
+    private static final String COLUMN_FAMILY = "document";
+    private static final String COLUMN_QUALIFIER = "dataset";
+
+    public DatasetRepository(BigtableDataClient dataClient) {
+        this.dataClient = dataClient;
+    }
+
+    /**
+     * Get the latest dataset with the given id
+     */
+    public Dataset get(String id) throws InvalidProtocolBufferException {
+        for (Row row : dataClient.readRows(Query.create(TABLE_ID).prefix(id).limit(1))) {
+            return Dataset.parseFrom(row.getCells(COLUMN_FAMILY, COLUMN_QUALIFIER).get(0).getValue());
+        }
+        return null;
+    }
+
+    public Dataset get(String id, long timestamp) {
+        throw new RuntimeException("Not implemented");
+    }
+
+
+    public void create(Dataset dataset) {
+
+        // We create a reverse timestamp so that the most recent dataset will appear at the start of the table instead
+        // of the end. This because we assume that the most common query will be for the latest dataset.
+        Long reverseTimestamp = Long.MAX_VALUE - System.currentTimeMillis();
+
+        RowMutation rowMutation = RowMutation.create(TABLE_ID, String.format("%s#%d", dataset.getId(), reverseTimestamp))
+                .setCell(
+                        COLUMN_FAMILY,
+                        ByteString.copyFrom(COLUMN_QUALIFIER.getBytes()),
+                        dataset.toByteString()
+                );
+
+        dataClient.mutateRow(rowMutation);
+    }
+
+    public void update(Dataset dataset) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void delete(String id) {
+        throw new RuntimeException("Not implemented");
+    }
+}
---
+++ service/src/test/java/no/ssb/dapla/catalog/repository/DatasetRepositoryTest.java
@@ -0,0 +1,91 @@
+package no.ssb.dapla.catalog.repository;
+
+import com.google.cloud.bigtable.admin.v2.BigtableTableAdminClient;
+import com.google.cloud.bigtable.admin.v2.BigtableTableAdminSettings;
+import com.google.cloud.bigtable.admin.v2.models.CreateTableRequest;
+import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
+import com.google.protobuf.ByteString;
+import no.ssb.dapla.catalog.protobuf.Dataset;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DatasetRepositoryTest {
+
+    void createTableIfNotExists(String tableId, String columnFamily) {
+
+        BigtableTableAdminSettings settings;
+        try {
+            settings = BigtableTableAdminSettings
+                    .newBuilderForEmulator("localhost", 9035)
+                    .setProjectId("my-project")
+                    .setInstanceId("my-instance")
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try (BigtableTableAdminClient adminClient = BigtableTableAdminClient.create(settings)) {
+
+            if (!adminClient.exists(tableId)) {
+                CreateTableRequest createTableRequest = CreateTableRequest.of(tableId).addFamily(columnFamily);
+                adminClient.createTable(createTableRequest);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void thatWriteWorks() throws IOException {
+
+        createTableIfNotExists("dataset", "document");
+
+        BigtableDataSettings settings = BigtableDataSettings
+                .newBuilderForEmulator("localhost", 9035)
+                .setProjectId("my-project")
+                .setInstanceId("my-instance")
+                .build();
+
+        Dataset ds1 = Dataset.newBuilder()
+                .setId("0162a6e9-a2c7-4079-82f5-f14b484b0fa9")
+                .setState(Dataset.DatasetState.RAW)
+                .setValuation(Dataset.Valuation.SHIELDED)
+                .addLocations("gcs://some-file")
+                .build();
+
+        Dataset ds2 = Dataset.newBuilder()
+                .setId("0162a6e9-a2c7-4079-82f5-f14b484b0fa9")
+                .setState(Dataset.DatasetState.INPUT)
+                .setValuation(Dataset.Valuation.INTERNAL)
+                .addLocations("gcs://another-file")
+                .build();
+
+        Dataset ds3 = Dataset.newBuilder()
+                .setId("a5c84237-dbea-4ec0-acb0-03c732bf8040")
+                .setState(Dataset.DatasetState.INPUT)
+                .setValuation(Dataset.Valuation.INTERNAL)
+                .addLocations("gcs://another-file")
+                .build();
+
+        try (BigtableDataClient client = BigtableDataClient.create(settings)) {
+
+            DatasetRepository repository = new DatasetRepository(client);
+            repository.create(ds1);
+            repository.create(ds2);
+            repository.create(ds3);
+
+            Dataset dataset = repository.get("0162a6e9-a2c7-4079-82f5-f14b484b0fa9");
+
+            assertThat(dataset.getId()).isEqualTo("0162a6e9-a2c7-4079-82f5-f14b484b0fa9");
+            assertThat(dataset.getState()).isEqualTo(Dataset.DatasetState.INPUT);
+            assertThat(dataset.getValuation()).isEqualTo(Dataset.Valuation.INTERNAL);
+            assertThat(dataset.getLocationsList().asByteStringList()).containsExactly(ByteString.copyFrom("gcs://another-file".getBytes()));
+        }
+    }
+}
