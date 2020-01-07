62b84761ea31aaa79029c5e47367e10379c0353c
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-01-07 15:58:59 +0100

    print part of service-key to confirm

--- service/src/main/java/no/ssb/dapla/catalog/BigtableInitializer.java
+++ service/src/main/java/no/ssb/dapla/catalog/BigtableInitializer.java
@@ -11,7 +11,10 @@ import io.helidon.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -112,6 +115,15 @@ public class BigtableInitializer {
 
     static BigtableDataClient createRealBigtableDataClient(Path serviceAccountKeyFilePath, String projectId, String instanceId) {
         try {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                    Files.newInputStream(serviceAccountKeyFilePath, StandardOpenOption.READ), StandardCharsets.UTF_8))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if (line.contains("private_key_id")) {
+                        LOG.info("From service-account-key file:\n{}", line);
+                    }
+                }
+            }
             GoogleCredentials credentials = ServiceAccountCredentials.fromStream(
                     Files.newInputStream(serviceAccountKeyFilePath, StandardOpenOption.READ))
                     .createScoped(Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
